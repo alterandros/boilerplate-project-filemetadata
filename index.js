32141c01fc7bd7b0109aa7da90f86ba30c8d6e10
Replit user <>
2023-12-06 19:55:57 +0000

    feat: user POST request to analyze file and upload to uploads folder

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 const multer = require('multer');
 var app = express();
 
+// Define file destination
+const upload = multer({ dest: 'uploads/' })
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +14,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+// POST request for user file analysis
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+})
 
 
 const port = process.env.PORT || 3000;
